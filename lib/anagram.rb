1548277ee8aa4025722fb767cef46e8c8408640c
jamilagastar <jamilagastar@gmail.com>
2022-08-03 19:55:38 +1000

    completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array_of_anagram)
+        array_of_anagram.select { |possible_anagram| possible_anagram.split("").sort == @word.split("").sort }
+    end
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
